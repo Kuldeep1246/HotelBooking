ff6c127cf80c2531a40956da959446a25b7e6e56
kuldeep1246 <kuldeepgangarekar2001@gmail.com>
2025-06-18 13:52:30 +0530

    Hotel Booking

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -12,7 +12,7 @@ const clerkWebhooks = async (req, res) => {
     const headers = {
       "svix-id": req.headers["svix-id"],
       "svix-timestamp": req.headers["svix-timestamp"],
-      "svix-signature": req.headers["svix-signature"],
+      "svix-signature": req.headers["svix-signature"]
     };
 
     // Verifying Headers
@@ -21,21 +21,26 @@ const clerkWebhooks = async (req, res) => {
     // Getting Data from request body
     const { data, type } = req.body;
 
-    const userData = {
-      _id: data.id,
-      email: data.email_addresses[0].email_address,
-      username: data.first_name + " " + data.last_name,
-      image: data.image_url,
-    };
-
     // Switch Cases for differernt Events
     switch (type) {
       case "user.created": {
+        const userData = {
+          _id: data.id,
+          email: data.email_addresses[0].email_address,
+          username: data.first_name + " " + data.last_name,
+          image: data.image_url
+        };
         await User.create(userData);
         break;
       }
 
       case "user.updated": {
+        const userData = {
+          _id: data.id,
+          email: data.email_addresses[0].email_address,
+          username: data.first_name + " " + data.last_name,
+          image: data.image_url
+        };
         await User.findByIdAndUpdate(data.id, userData);
         break;
       }
