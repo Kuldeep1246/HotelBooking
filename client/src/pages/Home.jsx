0765f90cd994382ec834436e47beed60060a89c7
kuldeep1246 <kuldeepgangarekar2001@gmail.com>
2025-06-18 13:50:03 +0530

    HotelBooking

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import FeaturedDestination from '../components/FeaturedDestination'
-import ExclusiveOffers from '../components/ExclusiveOffers'
-import Testimonial from '../components/Testimonial'
-import Footer from '../components/Footer'
+import ExclusiveOffers from "../components/ExclusiveOffers"
+import FeaturedDestination from "../components/FeaturedDestination"
+import Hero from "../components/Hero"
+import NewsLetter from "../components/NewsLetter"
+import RecommendedHotels from "../components/RecommendedHotels"
+import Testimonial from "../components/Testimonial"
 
-function Home() {
-  return (
-    <>
-    <Hero/>
-    <FeaturedDestination/>
-    <ExclusiveOffers/>
-    <Testimonial/>
-    </>
-  )
+
+const Home = () => {
+
+
+    return (
+        <>
+            <Hero />
+            <RecommendedHotels />
+            <FeaturedDestination />
+            <ExclusiveOffers />
+            <Testimonial />
+            <NewsLetter/>
+        </>
+    )
 }
 
 export default Home
